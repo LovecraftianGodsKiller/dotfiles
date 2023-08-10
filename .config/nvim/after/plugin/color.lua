c80fddffb4d839e50bba29f8b23c9e0b0c915485
LovecraftianGodsKiller <sandfromundertal@protonmail.com>
2023-08-10 10:38:46 -0700

    Added every configuration file

---
+++ .config/nvim/after/plugin/color.lua
@@ -0,0 +1,5 @@
+vim.g.catppuccin_transparent_sidebar = true
+vim.g.catppuccin_transparent = true
+vim.opt.background = "dark"
+
+vim.cmd("colorscheme catppuccin-mocha")
---
+++ .config/nvim/init.lua
@@ -0,0 +1,2 @@
+require("kirb.set")
+print("hello")
