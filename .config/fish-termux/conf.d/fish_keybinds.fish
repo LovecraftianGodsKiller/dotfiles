355b25db84e4b652eb038d7df2d88b2d4552e934
LovecraftianGodsKiller <sandfromundertal@protonmail.com>
2024-01-16 11:11:51 -0800

    Added fish keybind config file

---
+++ .config/fish-termux/conf.d/fish_keybinds.fish
@@ -0,0 +1,3 @@
+bind \e\[3\;5~ kill-word # bash and zsh (if oh-my-zsh is installed) ctrl+del to delete word to right of cursor | termux
+bind \e\[1\;3H backward-kill-line # delete everything to left of cursor | termux
+bind \e\[1\;3F kill-line # delete everything to right of cursor | termux
